0116a1e0b87887591c0217de228759edfb856b0f
Dav_Sot <david.sotunbo@e4email.net>
2020-01-27 10:19:42 +0100

    encryption of pasword and linking to login page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
 
 app= Flask(__name__)
 app.config['SECRET_KEY'] = 'd5c8bc8a808acef6421d75ba0621204300e4d8961e8aa33b4e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import route
