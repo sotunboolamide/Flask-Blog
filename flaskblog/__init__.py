71a0ae27eaa24c9a2a642faea7b2c4b2510ba438
Sotunbo David <david.sotunbo@e4email.net>
2020-01-28 19:41:24 +0100

    implemented logout route and page redirecting

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app= Flask(__name__)
 app.config['SECRET_KEY'] = 'd5c8bc8a808acef6421d75ba0621204300e4d8961e8aa33b4e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import route
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
                             validators=[DataRequired(), Length(min=2, max = 20)])
@@ -11,6 +11,19 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self,username):
+
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('That user name is taken. Please use another one')
+    
+
+    def validate_email(self,email):
+
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please use another one')
 
 
 class LoginForm(FlaskForm):
